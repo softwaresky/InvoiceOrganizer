3226b62daa14b0a2218e686412f1ff92b9788fed
softwaresky <doc.softwaresky@gmail.com>
2020-08-19 01:38:17 +0200

    fix

--- setup.py
+++ setup.py
@@ -8,24 +8,24 @@ def make_build(script_target=""):
     # dependencies
     build_exe_options = {
         "packages": ["lib.DbLib"],
-        "include_files": [(os.path.abspath("./db/SmetkovotstvenaKniga.db"), "db/SmetkovotstvenaKniga.db"),
+        "include_files": [(os.path.abspath("./db/InvoiceOrganizer.db"), "db/InvoiceOrganizer.db"),
                             (os.path.abspath("./stamp/img_stamp.png"), "stamp/img_stamp.png"),
                           (os.path.abspath("./html_template/html_a4_page.html"), "html_template/html_a4_page.html")],
         "excludes": ["Tkinter", "Tkconstants", "tcl", ],
         "includes": ["lib.DbLib"],
-        "build_exe": "E:/build_script/SmetkovotstvenaKniga-32"
+        "build_exe": "./build_script/InvoiceOrganizer-32"
     }
 
     executable = [
         Executable(r"{0}".format(script_target),
                    base="Win32GUI",
-                   targetName="SmetkovotstvenaKniga.exe")
+                   targetName="InvoiceOrganizer.exe")
     ]
 
     setup(
-        name="SmetkovotstvenaKniga",
+        name="InvoiceOrganizer",
         version="0.1",
-        description="SmetkovotstvenaKniga za Mama!",
+        description="InvoiceOrganizer for import / export invoices!",
         author="Softwaresky",
         options={"build_exe": build_exe_options},
         executables=executable
@@ -33,7 +33,7 @@ def make_build(script_target=""):
 
 def main():
     sys.argv.append("build")
-    script_target = os.path.abspath("./SmetkovotstvenaKnigaGUI.py")
+    script_target = os.path.abspath("./InvoiceOrganizerGUI.py")
     make_build(script_target=script_target)
 
 main()
